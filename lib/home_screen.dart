7e86f8746174b8b85256a73488fa749e97a725c3
Md. Abir Hasan <62834401+AbirHasanShuvo@users.noreply.github.com>
2026-02-01 17:09:34 +0600

    understand that consumerstateful widget does need to added a ref variable

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,15 +8,35 @@ final age = Provider<int>((ref) {
   return 27;
 });
 
-class HomeScreen extends ConsumerWidget {
+// class HomeScreen extends ConsumerWidget {
+//   const HomeScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final subscribe = ref.watch(hello);
+//     final temp = ref.watch(age);
+//     return Scaffold(
+//       body: Center(child: Text(subscribe, style: TextStyle(fontSize: 30))),
+//     );
+//   }
+// }
+
+//using it with stateful widget
+
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final subscribe = ref.watch(hello);
+  ConsumerState createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final subscribe = ref.watch(hello);
+    final temp = ref.watch(age);
     return Scaffold(
-      body: Center(child: Text(subscribe, style: TextStyle(fontSize: 30))),
+      body: Center(child: Text(subscribe +" "+ temp.toString(), style: TextStyle(fontSize: 30))),
     );
   }
 }
