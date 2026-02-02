c64be2fcb16a6b634f3797b36e540e01017017c9
Md. Abir Hasan <62834401+AbirHasanShuvo@users.noreply.github.com>
2026-02-02 11:32:09 +0600

    multiple state handling with riverpod is complered

---
+++ lib/slider/slider_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:state_project/slider/slider_provider.dart';
+
+class SliderScreen extends ConsumerWidget {
+  const SliderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final slider = ref.watch(sliderProvider);
+    print('build');
+    return Scaffold(
+      appBar: AppBar(title: Text('Slider Page'), centerTitle: true),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Consumer(
+            builder: (context, ref, child) {
+              print('build eye');
+              //when i used full state then it become accessible for both eye and also
+              //slider so i needed to specify and select the specific variable for it
+
+              final showPassword = ref.watch(
+                sliderProvider.select((state) => state.showPassword),
+              );
+              return InkWell(
+                onTap: () {
+                  final stateProvider = ref.watch(sliderProvider.notifier);
+                  stateProvider.state = stateProvider.state.copyWith(
+                    showPassword: !showPassword,
+                  );
+                },
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: showPassword
+                      ? Icon(Icons.remove_red_eye)
+                      : Icon(Icons.image),
+                ),
+              );
+            },
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              print('slider');
+
+              final slider = ref.watch(
+                sliderProvider.select((state) => state.slider),
+              );
+              return Container(
+                height: 200,
+                width: 200,
+                color: Colors.red.withOpacity(slider),
+              );
+            },
+          ),
+
+          //here where where used the ref.watch we should use this in the consumer child
+          Consumer(
+            builder: (context, ref, child) {
+              final slider = ref.watch(
+                sliderProvider.select((state) => state.slider),
+              );
+              return Slider(
+                value: slider,
+                onChanged: (value) {
+                  final stateProvider = ref.watch(sliderProvider.notifier);
+                  stateProvider.state = stateProvider.state.copyWith(
+                    slider: value,
+                  );
+                },
+              );
+            },
+          ),
+
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
